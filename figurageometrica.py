23ca23a6b5e8102a7037de0f5898e7727f534cc5
Sylvia Pintado <sylviapintadoc@gmail.com>
2025-11-21 01:14:03 -0500

    Add files via upload

--- figurageometrica.py
+++ figurageometrica.py
@@ -25,3 +25,15 @@ class FiguraGeometrica:
         else:
             raise ValueError("El alto debe ser mayor que 0")
 
+    def area(self):
+        return self._ancho * self._alto
+
+    def __str__(self):
+        return f"FiguraGeometrica(ancho={self._ancho}, alto={self._alto})"
+
+
+
+if __name__ == '__main__':
+    figura = FiguraGeometrica(6, 8)
+    print(figura)
+    print("Área:", figura.area())
